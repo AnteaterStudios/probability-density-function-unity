b5abdd2eee9818f614bad371657a3de5e0fe7e8e
AnteaterStudios <anteaterstudios.games@gmail.com>
2021-07-19 14:57:24 -0300

    Finish practical example on a moving cube following the curve.

---
+++ Probability Density Function in Unity/Assets/ProbabilityDensityFunction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProbabilityDensityFunction : MonoBehaviour
+{
+    public float Speed;
+    public float Amplitude;
+    public float Height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowCurve(Speed, Amplitude, Height));
+    }
+
+    IEnumerator FollowCurve(float speed, float amplitude, float height)
+    {
+        //function: e^(-(1/2)*((x/a)^2))*h
+
+        float initialPos = -5f;
+        this.transform.position = new Vector3(initialPos, 0, 0);
+
+        float x = initialPos;
+        float y;
+
+        while (true)
+        {
+            y = Mathf.Exp((-.5f) * Mathf.Pow( x / amplitude, 2)) * height;
+
+            Debug.Log(y);
+
+            x += Time.deltaTime * speed;
+
+            this.transform.position = new Vector3(x, y, this.transform.position.z);
+
+            yield return null;
+        }
+    }
+}
